4da1b7ba2a92c0218cd73c3b62072baeb3514cc7
Rebekah <rebekah.adair@yahoo.com>
2016-04-06 15:20:02 -0400

    router work, fixed bioview export

--- src/js/components/AppRouter.js
+++ src/js/components/AppRouter.js
@@ -5,7 +5,8 @@ var DashboardView = require('./Run/list/DashboardView');
 var NewRunPage = require('./Run/forms/NewRunPage');
 var dispatcher = require('./Events/dispatcher');
 var DetailView = require('./Run/list/DetailView');
-var BioView
+var BioView = require('./Run/forms/BioView');
+var ListItemView = require('./Run/list/ListItemView');
 
 var indexCollection = require('./Run/list/IndexCollection');
 
@@ -45,10 +46,12 @@ var AppRouter = Backbone.Router.extend({
 
     runs: function () {
         indexCollection.fetch({
-
+            success: function () {
+                dispatcher.trigger('show', new ListItemView({ collection: indexCollection }));
+            }
         });
     },
-
+  
     bio: function () {
         dispatcher.trigger('app:show', new BioView());
     }
