1e8baf2bab5c8fa73fb822ce05a3b32062d24d1b
Drew Sancho <drew_sancho@yahoo.com>
2016-04-07 14:07:24 -0400

    wrecked the whole damn thing

--- src/js/components/Run/list/DistanceView.js
+++ src/js/components/Run/list/DistanceView.js
@@ -9,7 +9,7 @@ var DistanceView = Backbone.View.extend({
     template: _.template(require('./distanceView.html')),
 
     events: {
-        'click .distance-run': 'onClick'
+        'click .click-here': 'onClick'
     },
 
     onClick: function () {
@@ -18,8 +18,12 @@ var DistanceView = Backbone.View.extend({
 
     render: function () {
         this.$el.html(this.template(this.model.attributes));
-    }
+        this.$el.find('.miles_bar').css({'width': this.getDistancePercent(this.model.get('runDistance'))});
+    },
 
+    getDistancePercent: function (distance) {
+        return Number(distance) * 6.7 + '%';
+    }
 });
 
 module.exports = DistanceView;
